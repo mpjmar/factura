c33252610935e901a7ed20ea42efc9ac24cd25cc
M. Paz Jimenez <meripeich@gmail.com>
2025-09-25 12:15:59 +0200

    feat: header alignment

--- src/App.java
+++ src/App.java
@@ -1,15 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Rojo Polo Paella Inc.");
+        System.out.println("");
         System.out.println("Carretera Muelle 38");
         System.out.println("37531 Ávila, Ávila");
         System.out.println("");
         System.out.println("");
         System.out.println("");
         System.out.printf("%-30s %-30s %-30s %30s %n", "FACTURAR A", "ENVIAR A ", "Nº FACTURA", "ES-001");
+        System.out.println("");
         System.out.printf("%-30s %-30s %-30s %30s %n", "Leda Villareal", "Leda Villareal", "FECHA", "ES-001");
-        System.out.printf("%-30s %-30s %-30s %30s %n", "Virgen Blanca 63", "Cercas Bajas 68", "Nº PEDIDO", "ES-001");
-        System.out.printf("%-30s %-30s %-30s %30s %n", "", "", "FECHA VCTO.", "29-01-2019");
+        System.out.printf("%-30s %-30s %-30s %30s %n", "Virgen Blanca 63", "Cercas Bajas 68", "Nº PEDIDO", "1730/2019");
+        System.out.printf("%-30s %-30s %-30s %30s %n", "08759 Burgos, Burgos", "47300 Cádiz, Cádiz", "FECHA VCTO.", "29.01.2019");
         System.out.println("");
         System.out.printf("───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────%n");
         System.out.printf("%-30s %-30s %-30s %30s %n", "Total Factura", "", "", "199,65");
